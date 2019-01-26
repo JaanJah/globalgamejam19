f620f7bcede9d7cba045c15fb15e7d614bea1a38
KaurSamuel <Kaursamuel.kannel@tptlive.ee>
2019-01-26 17:25:40 +0200

    Door keys and locked doors work

--- Labyrinth/Assets/Scripts/Player.cs
+++ Labyrinth/Assets/Scripts/Player.cs
@@ -9,24 +9,61 @@ public class Player : MonoBehaviour {
     public Text CollectedItems;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        string Uitext = "";
         if (collision.gameObject.tag == "ObjectToCollect")
         {
             Inventory.Add(collision.gameObject.name);
-            CollectedItems.text = Inventory.ToString();
+            
+            foreach (var item in Inventory)
+            {
+                Uitext += item +", ";
+            }
+            CollectedItems.text = Uitext;
             Destroy(collision.gameObject);
             
         }
 
-        foreach (var item in Inventory)
+        if (collision.gameObject.tag == "GreenDoor")
         {
-            if (item == "purpleKey" && collision.gameObject.name == "purpleDoor" ||
-                item == "greenKey" && collision.gameObject.name == "greenDoor")
+            if (Inventory.Contains("greenKey"))
             {
                 Destroy(collision.gameObject);
-                Inventory.Remove(item);
-                //TODO: Remove the items from CollectedItems.text when this if statement occurs
+                Inventory.Remove("greenKey");
+                foreach (var item in Inventory)
+                {
+                    Uitext += item + ", ";
+                }
+                CollectedItems.text = Uitext;
             }
         }
+
+        if (collision.gameObject.tag == "PurpleDoor")
+        {
+            if (Inventory.Contains("purpleKey"))
+            {
+                Destroy(collision.gameObject);
+                Inventory.Remove("purpleKey");
+                foreach (var item in Inventory)
+                {
+                    Uitext += item + ", ";
+                }
+                CollectedItems.text = Uitext;
+            }
+        }
+        //foreach (var item in Inventory)
+        //{
+        //    if (item == "purpleKey" && collision.gameObject.name == "purpleDoor" ||
+        //        item == "greenKey" && collision.gameObject.name == "greenDoor")
+        //    {
+        //        Inventory.Remove(item);
+        //        foreach (var item2 in Inventory)
+        //        {
+        //            Uitext += item2 + ", ";
+        //        }
+        //        Destroy(collision.gameObject);
+        //        //TODO: Remove the items from CollectedItems.text when this if statement occurs
+        //    }
+        //}
     }
     // Use this for initialization
     void Start () {
