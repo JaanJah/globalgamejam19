e8fe0e1a67d5b742116b8c78b55e86e0ef6b680f
KasparRosin <kaspar.rosin@tptlive.ee>
2019-01-27 03:02:08 +0200

    some highscores

--- Labyrinth/Assets/Scenes/GetSetWrite.cs
+++ Labyrinth/Assets/Scenes/GetSetWrite.cs
@@ -9,17 +9,27 @@ public class GetSetWrite : MonoBehaviour
 {
     public Text Nickname;
     public Text Warning;
-
+    public Text HiScores;
+    public GameObject HiScores2;
     public GameObject Input2;
+    public GameObject Button2;
     public InputField Input;
     public Button Button;
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        HiScores2.SetActive(false);
+    }
     public void GetSet()
     {
         if (Nickname.text != "")
         {
             File.AppendAllText(@"../scores.txt", Nickname.text + Environment.NewLine);
-            //Input2.SetActive(false);
+            Input2.SetActive(false);
+            Button2.SetActive(false);
+            HiScores2.SetActive(true);
+            GetScores();
         }
         else
         {
@@ -27,4 +37,25 @@ public class GetSetWrite : MonoBehaviour
         }
         
     }
+
+    public void GetScores()
+    {
+        var Laps = 1;
+        HiScores.text = "Hiscores:\n";
+        var lines = File.ReadAllLines(@"../scores.txt");
+        foreach (var line in lines)
+        {
+            
+            if (Laps == lines.Length)
+            {
+                HiScores.text += "\nYour Score:\n" + line.ToString(); 
+            }
+            else
+            {
+                HiScores.text += line.ToString() + "\n";
+                Laps++;
+            }            
+        }
+    }
+
 }
